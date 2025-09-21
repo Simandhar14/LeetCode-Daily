9a2e944fa7f7699af5fb0dc128de3fc4cf681349
Simandhar <121376696+Simandhar14@users.noreply.github.com>
2025-09-21 18:44:54 +0530

    Update 1912.cpp

--- 1912.cpp
+++ 1912.cpp
@@ -72,3 +72,62 @@ public:
  * obj->drop(shop,movie);
  * vector<vector<int>> param_4 = obj->report();
  */
+
+
+//better approach with less space
+class MovieRentingSystem {
+public:
+    unordered_map<int, set<vector<int>>> movieMap;
+    set<vector<int>> rented;                       
+    unordered_map<string, int> priceMap;           
+
+    MovieRentingSystem(int n, vector<vector<int>>& entries) {
+        for (auto &temp : entries) {
+            int shop = temp[0];
+            int movie = temp[1];
+            int price = temp[2];
+            movieMap[movie].insert({price, shop});
+            string ss = to_string(movie) + "_" + to_string(shop);
+            priceMap[ss] = price;
+        }
+    }
+
+    vector<int> search(int movie) {
+        vector<int> ans;
+        int count = 0;
+        for (auto it = movieMap[movie].begin(); it != movieMap[movie].end() && count < 5; ++it) {
+            int price = (*it)[0];
+            int shop = (*it)[1];
+                ans.push_back(shop);
+                count++;
+        }
+        return ans;
+    }
+
+    void rent(int shop, int movie) {
+        string ss = to_string(movie) + "_" + to_string(shop);
+        int price = priceMap[ss];
+        rented.insert({price, shop, movie});
+        movieMap[movie].erase({price,shop});
+    }
+
+    void drop(int shop, int movie) {
+        string ss = to_string(movie) + "_" + to_string(shop);
+        int price = priceMap[ss];
+        rented.erase({price, shop, movie});
+        movieMap[movie].insert({price,shop});
+    }
+
+    vector<vector<int>> report() {
+        vector<vector<int>> ans;
+        int count = 0;
+        for (auto it = rented.begin(); it != rented.end() && count < 5; ++it) {
+            int shop = (*it)[1];
+            int movie = (*it)[2];
+            ans.push_back({shop, movie});
+            count++;
+        }
+        return ans;
+    }
+};
+
