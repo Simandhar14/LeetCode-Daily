201dcc360cc23b016858c5e76b50f484207abfe7
Simandhar <121376696+Simandhar14@users.noreply.github.com>
2025-10-14 19:28:29 +0530

    Update 3349.cpp

--- 3349.cpp
+++ 3349.cpp
@@ -32,3 +32,30 @@ public:
         return false;
     }
 };
+
+//optimal approach
+class Solution {
+public:
+    bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        int subarraySize = 1;
+        int l1 = -1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                subarraySize++;
+            } else {
+                if (l1 >= 2 * k || subarraySize >= 2 * k)
+                    return true;
+                else if (l1 >= k && subarraySize >= k)
+                    return true;
+                l1 = subarraySize;
+                subarraySize = 1;
+            }
+        }
+        if (l1 >= 2 * k || subarraySize >= 2 * k)
+            return true;
+        else if (l1 >= k && subarraySize >= k)
+            return true;
+        return false;
+    }
+};
