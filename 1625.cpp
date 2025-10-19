3001da499029873ac36fc357f5430a4aed769092
Simandhar <121376696+Simandhar14@users.noreply.github.com>
2025-10-19 15:31:24 +0530

    Implement BFS for lexicographically smallest string

--- 1625.cpp
+++ 1625.cpp
@@ -27,3 +27,45 @@ public:
         return result;
     }
 };
+
+//bfs
+class Solution {
+public:
+    string rotate(string ss, int b) {
+        string s=ss;
+        reverse(begin(s), end(s));
+        reverse(begin(s), begin(s) + b);
+        reverse(begin(s) + b, end(s));
+        return s;
+    }
+    string findLexSmallestString(string s, int a, int b) {
+        int n = s.size();
+        unordered_set<string> seen;
+        string smallest = s;
+        queue<string> que;
+        que.push(s);
+        seen.insert(s);
+        while (!que.empty()) {
+            string temp = que.front();
+            que.pop();
+            if (temp < smallest)
+                smallest = temp;
+            string add = temp;
+            for (int i = 1; i < s.size(); i += 2) {
+                int num = add[i] - '0';
+                int newnum = (num + a) % 10;
+                add[i] = newnum + '0';
+            }
+            if (!seen.count(add)) {
+                seen.insert(add);
+                que.push(add);
+            }
+            temp=rotate(temp, b);
+            if (!seen.count(temp)) {
+                seen.insert(temp);
+                que.push(temp);
+            }
+        }
+        return smallest;
+    }
+};
