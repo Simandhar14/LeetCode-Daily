f67690d126f07f166b2c5a7218748410182aa459
Simandhar <121376696+Simandhar14@users.noreply.github.com>
2025-10-24 21:00:53 +0530

    Update 2048.cpp

--- 2048.cpp
+++ 2048.cpp
@@ -34,3 +34,62 @@ public:
         return -1;
     }
 };
+
+//backtracking
+class Solution {
+public:
+    int N;
+
+    int solve(int digitsleft, string freq, int num) {
+        if (digitsleft == 0) {
+            if (num > N) {
+                bool correct = true;
+                for (int i = 0; i <= 9; i++) {
+                    if (freq[i] == '0')
+                        continue;
+                    if (i != freq[i] - '0') {
+                        correct = false;
+                        break;
+                    }
+                }
+                if (correct)
+                    return num;
+                else
+                    return 0;
+            } else
+                return 0;
+        }
+        for (int i = 1; i <= 9; i++) {
+            int currentfreq = freq[i] - '0';
+            if (currentfreq > 0 && currentfreq <= digitsleft) {
+                int newnum = num * 10 + i;
+                freq[i]--;
+                int res = solve(digitsleft - 1, freq, newnum);
+                if (res != 0)
+                    return res;
+                freq[i]++;
+            }
+        }
+        return 0;
+    }
+    int nextBeautifulNumber(int n) {
+        N = n;
+        int digits = 0;
+        int number = n;
+        while (number > 0) {
+            digits++;
+            number /= 10;
+        }
+        string freq = string(10, '0');
+        for (int i = 0; i < 10; i++) {
+            freq[i] = i + '0';
+        }
+
+        int result=solve(digits, freq, 0);
+        if(result==0)
+        {
+            result=solve(digits+1, freq, 0);
+        }
+        return result;
+    }
+};
