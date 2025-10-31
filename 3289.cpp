1c51376b870a05ab0c8d590c27329da2b354e16e
Simandhar <121376696+Simandhar14@users.noreply.github.com>
2025-10-31 15:34:10 +0530

    Update 3289.cpp

--- 3289.cpp
+++ 3289.cpp
@@ -15,3 +15,21 @@ public:
         return sneakyNumbers;
     }
 };
+
+//without extra space
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        vector<int>ans;
+        int n=nums.size();
+        for(int &i:nums){
+            i+=n;
+        }
+        for(int i=0;i<nums.size();i++){
+            int index=abs(nums[i])%n;
+            if(nums[index]<0) ans.push_back(abs(nums[i])%n);
+            else nums[index]*=-1;
+        }
+        return ans;
+    }
+};
