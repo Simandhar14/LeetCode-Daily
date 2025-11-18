b77615b990e570daec0b731dd3db85e87564d0e3
Simandhar <121376696+Simandhar14@users.noreply.github.com>
2025-11-18 11:57:48 +0530

    Implement isOneBitCharacter function in Solution class

---
+++ 717.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int n=bits.size();
+        int i=0;
+        while(i<n)
+        {
+            if(i==n-1 && bits[i]==0) return true;
+            if(bits[i]==1) i+=2;
+            else i+=1;
+        }
+        return false;
+    }
+};
