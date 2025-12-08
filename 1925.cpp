665b5a004e256e9cadc809440be8b749ab027624
Simandhar <121376696+Simandhar14@users.noreply.github.com>
2025-12-08 17:22:07 +0530

    Update 1925.cpp

--- 1925.cpp
+++ 1925.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
-int power(int a,int b)
-{
-    if(b==0) return 1;
-    int half=power(a,b/2);
-    int result=half*half;
-    if(b%2==1) result*=a;
-    return result;
-}
     int countTriples(int n) {
         int count=0;
-        for(int i=1;i<=n;i++)
+        for(int a=1;a<=n;a++)
         {
-            for(int j=i;j<=n;j++)
+            for(int b=a;b<=n;b++)
             {
-                for(int k=i;k<=n;k++)
+                for(int c=a;c<=n;c++)
                 {
-                    int a=power(i,2);
-                    int b=power(j,2);
-                    int c=power(k,2);
-                    if((a+b)==c) count+=2;
+                    if(a==sqrt((c+b) * (c-b))) count+=2;
                 }
             }
         }
