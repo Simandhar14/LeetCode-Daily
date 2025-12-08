c733b37ef709fc7768f8a67356f6adca510f02cb
Simandhar <121376696+Simandhar14@users.noreply.github.com>
2025-12-08 17:24:45 +0530

    Update 1925.cpp

--- 1925.cpp
+++ 1925.cpp
@@ -15,3 +15,26 @@ public:
         return count;
     }
 };
+
+//using extra space but better time complexity
+
+class Solution {
+public:
+    int countTriples(int n) {
+        int count=0;
+        unordered_set<int>a2;
+        for(int a=1;a<=n;a++)
+        {
+            a2.insert(a*a);
+        }
+        for(int b=1;b<=n;b++)
+        {
+            for(int c=b;c<=n;c++)
+            {
+                int multiply=(c+b) * (c-b);
+                if(a2.find(multiply)!=a2.end()) count+=1;
+            }
+        }
+        return count;
+    }
+};
