028a3c3f1804842ec0a160f489cc195d14999132
Simandhar <121376696+Simandhar14@users.noreply.github.com>
2025-12-19 17:47:45 +0530

    Update 2092.cpp

--- 2092.cpp
+++ 2092.cpp
@@ -1,3 +1,4 @@
+//dfs
 class Solution {
 public:
     void dfs(unordered_map<int, vector<int>>& adj,
@@ -49,3 +50,63 @@ public:
         return result;
     }
 };
+
+//bfs
+class Solution {
+public:
+    void bfs(unordered_map<int, vector<int>>& adj, vector<bool>& secretPeople,
+             int u, unordered_set<int>& visited) {
+
+        visited.insert(u);
+        queue<int> que;
+        que.push(u);
+        while (!que.empty()) {
+            auto u = que.front();
+            que.pop();
+            for (int& v : adj[u]) {
+                if (!visited.count(v)) {
+                    secretPeople[v] = true;
+                    que.push(v);
+                    visited.insert(v);
+                }
+            }
+        }
+    }
+
+    vector<int> findAllPeople(int n, vector<vector<int>>& meetings,
+                              int firstPerson) {
+
+        vector<bool> secretPeople(n, false);
+        secretPeople[0] = true;
+        secretPeople[firstPerson] = true;
+
+        map<int, vector<pair<int, int>>> mp;
+        for (auto& temp : meetings) {
+            mp[temp[2]].push_back({temp[0], temp[1]});
+        }
+
+        for (auto& it : mp) {
+            unordered_map<int, vector<int>> adj;
+            unordered_set<int> visited;
+
+            for (auto& p : it.second) {
+                adj[p.first].push_back(p.second);
+                adj[p.second].push_back(p.first);
+            }
+
+            for (auto& p : it.second) {
+                if (secretPeople[p.first] && !visited.count(p.first))
+                    bfs(adj, secretPeople, p.first, visited);
+                if (secretPeople[p.second] && !visited.count(p.second))
+                    bfs(adj, secretPeople, p.second, visited);
+            }
+        }
+
+        vector<int> result;
+        for (int i = 0; i < n; i++)
+            if (secretPeople[i])
+                result.push_back(i);
+        return result;
+    }
+};
+
