ed5d8eb9e1e190812da19839983798e2e1e4e825
Simandhar <121376696+Simandhar14@users.noreply.github.com>
2026-01-10 18:26:28 +0530

    Update 712.cpp

--- 712.cpp
+++ 712.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
-int dp[1001][1001];
+    int m, n;
+    
+    int t[1001][1001];
+    
     int solve(string &s1, string &s2, int i, int j) {
-        if (i >= s1.size() || j >= s2.size())
-        {
+        if(i >= m && j >= n)
             return 0;
+        
+        if(t[i][j] != -1)
+            return t[i][j];
+        
+        if(i >= m) {
+            return t[i][j] = s2[j] + solve(s1, s2, i, j+1);
+        } else if(j >= n) {
+            return t[i][j] = s1[i] + solve(s1, s2, i+1, j);
         }
-        if(dp[i][j]!=-1) return dp[i][j];
-        int take=0,skip1=0,skip2=0;
-        if(s1[i]==s2[j])
-        {
-            take+=(2*((s1[i]-'a')+97)+solve(s1,s2,i+1,j+1));
-        }
-        skip1=solve(s1,s2,i+1,j);
-        skip2=solve(s1,s2,i,j+1);
-        return dp[i][j]=max({take,skip1,skip2});
+        
+        if(s1[i] == s2[j])
+            return t[i][j] = solve(s1, s2, i+1, j+1);
+        
+        int take_s1_i = s1[i] + solve(s1, s2, i+1, j);
+        int take_s2_j = s2[j] + solve(s1, s2, i, j+1);
+        
+        return t[i][j] = min(take_s1_i, take_s2_j);
     }
+    
     int minimumDeleteSum(string s1, string s2) {
-        int totalsum = 0;
-        for (int i = 0; i < s1.size(); i++) {
-            totalsum += (s1[i] - 'a')+97;
-        }
-        for (int j = 0; j < s2.size(); j++) {
-            totalsum += (s2[j] - 'a')+97;
-        }
-        memset(dp,-1,sizeof(dp));
-        return totalsum-solve(s1,s2,0,0);
+        m = s1.length();
+         n = s2.length();
+        
+        memset(t, -1, sizeof(t));
+        
+        return solve(s1, s2, 0, 0);
     }
 };
