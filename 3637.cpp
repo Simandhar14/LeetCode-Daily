5f3da9feba49ae0931175759cd92826f127ef349
Simandhar <121376696+Simandhar14@users.noreply.github.com>
2026-02-03 12:12:10 +0530

    Create 3637.cpp

---
+++ 3637.cpp
@@ -0,0 +1,78 @@
+//cpp
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+        int n = nums.size();
+        for (int p = 1; p < n - 2; p++) {
+            for (int q = p + 1; q < n-1; q++) {
+                bool increasing = true;
+                for (int idx = 0; idx < p; idx++) {
+                    if (nums[idx] >= nums[idx + 1]) {
+                        increasing = false;
+                        break;
+                    }
+                }
+                if (!increasing)
+                    continue;
+                bool decreasing = true;
+                for (int idx = p; idx < q; idx++) {
+                    if (nums[idx] <= nums[idx + 1]) {
+                        decreasing = false;
+                        break;
+                    }
+                }
+                if (!decreasing)
+                    continue;
+                for (int idx = q; idx < n - 1; idx++) {
+                    if (nums[idx] >= nums[idx + 1]) {
+                        increasing = false;
+                        break;
+                    }
+                }
+                if (!increasing)
+                    continue;
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+//java
+class Solution {
+    public boolean isTrionic(int[] nums) {
+         int n = nums.length;
+        for (int p = 1; p < n - 2; p++) {
+            for (int q = p + 1; q < n-1; q++) {
+                boolean increasing = true;
+                for (int idx = 0; idx < p; idx++) {
+                    if (nums[idx] >= nums[idx + 1]) {
+                        increasing = false;
+                        break;
+                    }
+                }
+                if (!increasing)
+                    continue;
+                boolean decreasing = true;
+                for (int idx = p; idx < q; idx++) {
+                    if (nums[idx] <= nums[idx + 1]) {
+                        decreasing = false;
+                        break;
+                    }
+                }
+                if (!decreasing)
+                    continue;
+                for (int idx = q; idx < n - 1; idx++) {
+                    if (nums[idx] >= nums[idx + 1]) {
+                        increasing = false;
+                        break;
+                    }
+                }
+                if (!increasing)
+                    continue;
+                return true;
+            }
+        }
+        return false;
+    }
+}
