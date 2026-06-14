8103936e2e340ba909202fd5a0e70f7755243e84
Simandhar <121376696+Simandhar14@users.noreply.github.com>
2026-06-14 14:05:53 +0530

    Create 2130.cpp

---
+++ 2130.cpp
@@ -0,0 +1,27 @@
+//cpp
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        vector<int> ans;
+        while (head) {
+            ans.push_back(head->val);
+            head = head->next;
+        }
+        int result = 0;
+        int n = ans.size();
+        for (int i = 0; i < n / 2; i++) {
+            result = max(result, ans[i] + ans[n - 1 - i]);
+        }
+        return result;
+    }
+};
