2171dfe99c08ace210369846da66900c47f47907
Simandhar <121376696+Simandhar14@users.noreply.github.com>
2026-06-23 17:07:30 +0530

    Update 3699.cpp

--- 3699.cpp
+++ 3699.cpp
@@ -30,3 +30,73 @@ public:
         return solve(n, l, r, -1, -1, dp) % M;
     }
 };
+
+//bottom up dp
+class Solution {
+public:
+    static const int M = 1e9 + 7;
+
+    int zigZagArrays(int n, int l, int r) {
+        int mx = r + 1; 
+
+        vector<vector<vector<int>>> dp(
+            n + 1,
+            vector<vector<int>>(mx + 1, vector<int>(mx + 1, 0)));
+
+        for (int first = 0; first <= mx; first++) {
+            for (int second = 0; second <= mx; second++) {
+                dp[0][first][second] = 1;
+            }
+        }
+
+        for (int len = 1; len <= n; len++) {
+            for (int first = 0; first <= mx; first++) {
+                for (int second = 0; second <= mx; second++) {
+
+                    long long take = 0;
+
+                    for (int num = l; num <= r; num++) {
+                        int encNum = num + 1;
+
+                        if (first == 0) {
+                            take =
+                                (take +
+                                 dp[len - 1][encNum][second]) %
+                                M;
+                        }
+                        else if (second == 0) {
+                            int actualFirst = first - 1;
+
+                            if (num != actualFirst) {
+                                take =
+                                    (take +
+                                     dp[len - 1][first][encNum]) %
+                                    M;
+                            }
+                        }
+                        else {
+                            int actualFirst = first - 1;
+                            int actualSecond = second - 1;
+
+                            if (num != actualSecond &&
+                                !((actualFirst < actualSecond &&
+                                   actualSecond < num) ||
+                                  (actualFirst > actualSecond &&
+                                   actualSecond > num))) {
+
+                                take =
+                                    (take +
+                                     dp[len - 1][second][encNum]) %
+                                    M;
+                            }
+                        }
+                    }
+
+                    dp[len][first][second] = take;
+                }
+            }
+        }
+
+        return dp[n][0][0];
+    }
+};
