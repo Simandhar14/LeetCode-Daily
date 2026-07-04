612e5e40857b494238a29f2d98b42a892fc646e3
Simandhar <121376696+Simandhar14@users.noreply.github.com>
2026-07-04 13:48:58 +0530

    Create 2492.cpp

---
+++ 2492.cpp
@@ -0,0 +1,34 @@
+//cpp
+class Solution {
+public:
+    typedef pair<int, int> p;
+    int bfs(int u, unordered_map<int, vector<pair<int, int>>>& mp, int n) {
+        vector<int> result(n + 1, INT_MAX);
+        queue<int> que;
+        que.push(1);
+        while (!que.empty()) {
+            auto u = que.front();
+            que.pop();
+            for (auto& vec : mp[u]) {
+                int v = vec.first;
+                int cost = vec.second;
+                if (cost < result[v]) {
+                    result[v] = cost;
+                    que.push(v);
+                }
+            }
+        }
+        return *min_element(begin(result), end(result));
+    }
+    int minScore(int n, vector<vector<int>>& roads) {
+        unordered_map<int, vector<p>> mp;
+        for (auto& road : roads) {
+            int u = road[0];
+            int v = road[1];
+            int cost = road[2];
+            mp[u].push_back({v, cost});
+            mp[v].push_back({u, cost});
+        }
+        return bfs(1, mp, n);
+    }
+};
