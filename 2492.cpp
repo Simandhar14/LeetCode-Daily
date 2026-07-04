13bac701507c1f98553bae4109ed59c540370630
Simandhar <121376696+Simandhar14@users.noreply.github.com>
2026-07-04 13:53:09 +0530

    Update 2492.cpp

--- 2492.cpp
+++ 2492.cpp
@@ -1,4 +1,4 @@
-//cpp
+//BFS
 class Solution {
 public:
     typedef pair<int, int> p;
@@ -30,5 +30,35 @@ public:
             mp[v].push_back({u, cost});
         }
         return bfs(1, mp, n);
+    }
+};
+
+//DFS
+class Solution {
+public:
+    typedef pair<int, int> p;
+    void dfs(int u, unordered_map<int, vector<pair<int, int>>>& mp,
+             vector<int>& result) {
+        for (auto& vec : mp[u]) {
+            int v = vec.first;
+            int cost = vec.second;
+            if (cost < result[v]) {
+                result[v] = cost;
+                dfs(v, mp, result);
+            }
+        }
+    }
+    int minScore(int n, vector<vector<int>>& roads) {
+        unordered_map<int, vector<p>> mp;
+        for (auto& road : roads) {
+            int u = road[0];
+            int v = road[1];
+            int cost = road[2];
+            mp[u].push_back({v, cost});
+            mp[v].push_back({u, cost});
+        }
+        vector<int> result(n + 1, INT_MAX);
+        dfs(1, mp, result);
+        return *min_element(begin(result), end(result));
     }
 };
