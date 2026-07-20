4fff8411267b971d07eba73be3776e4fcaf0a0c1
Simandhar <121376696+Simandhar14@users.noreply.github.com>
2026-07-20 16:48:06 +0530

    Create 1260.cpp

---
+++ 1260.cpp
@@ -0,0 +1,45 @@
+//brute force
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
+        k = k % (m * n);
+        vector<int> start;
+        vector<vector<int>> result = grid;
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (k == 0)
+                    break;
+                start.push_back(grid[i][j]);
+                k--;
+            }
+        }
+        int row = 0, col = 0;
+        while (!start.empty()) {
+            result[row][col] = start.back();
+            start.pop_back();
+            col++;
+            if (col == n) {
+                col = 0;
+                row++;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row == m)
+                    break;
+                cout << row << " " << col << endl;
+                result[row][col] = grid[i][j];
+                col++;
+                if (col == n) {
+                    col = 0;
+                    row++;
+                }
+                if (row == m)
+                    break;
+            }
+        }
+        return result;
+    }
+};
