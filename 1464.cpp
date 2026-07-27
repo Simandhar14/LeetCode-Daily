fc4a864f55048988df4da78548349f91d273a70c
Simandhar <121376696+Simandhar14@users.noreply.github.com>
2026-07-27 20:18:10 +0530

    Optimize maxProduct function for better performance

--- 1464.cpp
+++ 1464.cpp
@@ -2,17 +2,17 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int maxnum = *max_element(begin(nums), end(nums));
-        vector<int> freq(maxnum + 1, 0);
-        for (int& num : nums) {
-            freq[num]++;
+        int first = 0, second = 0;
+
+        for (int x : nums) {
+            if (x >= first) {
+                second = first;
+                first = x;
+            } else if (x > second) {
+                second = x;
+            }
         }
-        if (freq[maxnum] > 1)
-            return (maxnum - 1) * (maxnum - 1);
-        for (int i = maxnum - 1; i >= 0; i--) {
-            if (freq[i] > 0)
-                return (maxnum - 1) * (i - 1);
-        }
-        return -1;
+
+        return (first - 1) * (second - 1);
     }
 };
