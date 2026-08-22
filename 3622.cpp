c1788a63733a8596b26f69fe6527651896a71f5f
Simandhar <121376696+Simandhar14@users.noreply.github.com>
2026-08-22 10:56:24 +0530

    Create 3622.cpp

---
+++ 3622.cpp
@@ -0,0 +1,17 @@
+//cpp
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+        int sum = 0;
+        int product = 1;
+        int temp = n;
+        while (temp > 0) {
+            int d = temp % 10;
+            sum += d;
+            product *= d;
+            temp /= 10;
+        }
+        int total = sum + product;
+        return n % total == 0;
+    }
+};
